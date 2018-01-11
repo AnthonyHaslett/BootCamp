0048d57e7e7656009a5b8c2b93afe8f573709bdc
naserAlomari <35262505+naserAlomari@users.noreply.github.com>
2018-01-11 14:31:09 +0000

    New push
    MySQL

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<html lang="en-US">
 <?php require_once('DatabaseConnection.php')?>
 <?php
 $database = new DatabaseConnection();
@@ -9,10 +10,16 @@ $values =$database->getConncetion()->prepare("SELECT `Unattended` FROM Attendanc
 $values->execute();
 $unAttended = $values->fetch()[0];
 ?>
-<html lang="en-US">
+
 <body>
 
-<h1>My Web Page</h1>
+<h1>Graphical Statistics</h1>
+
+
+
+<!--Create buttons that call the methods for drawing each chart-->
+<button onclick="drawAttendanceChart()">Attendance chart</button>
+<button onclick="drawAttendeeChart()">Attendee chart</button>
 
 <div id="barChart"></div>
 
@@ -20,27 +27,62 @@ $unAttended = $values->fetch()[0];
 
 <script type="text/javascript">
     // Load google charts
+
+
     google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
+    // google.charts.setOnLoadCallback(drawChart);
 
-    // Draw the chart and set the chart values
-    function drawChart() {
+    // Draw the chart and set the chart values for the attendance chart
+    function drawAttendanceChart() {
         var data = google.visualization.arrayToDataTable([
-            ['Task', 'Individuals Attending'],
-            ['Attended', <?php echo $attended?>],
+            ['Type', 'Attended', 'Not attending'],
 
-            ['Unattended', <?php
-                echo $unAttended?>]
+            ['Attended',  <?php echo $attended?>,  <?php echo $attended?>],
+            ['Unattended', <?php echo $attended?>, <?php echo $unAttended?> ]
+
+
         ]);
 
         // Optional; add a title and set the width and height of the chart
-        var options = {'title':'My Average Day', 'width':900, 'height':900};
+        var options = {'title':'Event Attendance ',
+            'width':900,
+            'height':900,
+            isStacked: true
+        };
+
 
         // Display the chart inside the <div> element with id="piechart"
         var chart = new google.visualization.ColumnChart(document.getElementById('barChart'));
         chart.draw(data, options);
+
+        //Filtering buttons
+//        var btn1 = document.createElement("BUTTON");
+//        btn1.innerHTML = "pie chart";
+//        document.body.appendChild(btn1);
+//     //   btn1.addEventListener ("click", drawAttendancePieChart());
+    }
+
+
+    // Draw the chart and set the chart values for the attendee chart
+    function drawAttendeeChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Type', 'Boys', 'Girls', 'Other'],
+            ['Under 14s', 2, 3, 1],
+            ['Over 14s', 1, 3, 0]
+
+
+
+        ]);
+
+        // Optional; add a title and set the width and height of the chart
+        var options = {'title':'Attendees ', isStacked: true, 'width':900, 'height':900};
+
+        // Display the chart inside the <div> element with id="piechart"
+        var chart = new google.visualization.ColumnChart(document.getElementById('barChart'));
+        chart.draw(data, options);
+
     }
 </script>
+
 </body>
 </html>
-
