1f57392d343e201bd6fd63561f1f0ca85ccc10f5
naserAlomari <35262505+naserAlomari@users.noreply.github.com>
2018-01-11 10:49:06 +0000

    Uploaded Origin Chart
    Chart code

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: stc954
- * Date: 10/01/2018
- * Time: 10:55
- */
+<!DOCTYPE html>
+<html lang="en-US">
+<body>
 
-echo "pass";
+<h1>My Web Page</h1>
 
+<div id="barChart"></div>
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+<script type="text/javascript">
+    // Load google charts
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    // Draw the chart and set the chart values
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Individuals Attending'],
+            ['Attended', 8],
+            ['Unattended', 2],
+
+        ]);
+
+        // Optional; add a title and set the width and height of the chart
+        var options = {'title':'My Average Day', 'width':900, 'height':900};
+
+        // Display the chart inside the <div> element with id="piechart"
+        var chart = new google.visualization.ColumnChart(document.getElementById('barChart'));
+        chart.draw(data, options);
+    }
+</script>
+
+</body>
+</html>
